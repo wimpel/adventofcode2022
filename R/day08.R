2e3ee02d96f04656c499a32e0f7fd902877b2642
wimbreedveld <wim.breedveld@gmail.com>
2022-12-08 14:14:25 +0100

    day 08

---
+++ R/day08.R
@@ -0,0 +1,44 @@
+library(data.table)
+# read and parse data
+temp <- fread("./input/day08.txt", header = FALSE, sep = "")
+DT <- setDT(lapply(tstrsplit(temp$V1, ""), as.numeric))
+
+# part 1
+# from top
+top <- DT[, lapply(.SD, function(x) !duplicated(cummax(x)))]
+# from bottom
+bottom <- DT[nrow(DT):1, lapply(.SD, function(x) !duplicated(cummax(x)))][nrow(DT): 1, ]
+# from left
+left <- transpose(transpose(DT)[, lapply(.SD, function(x) !duplicated(cummax(x)))])
+# from right
+right <- transpose(transpose(DT)[nrow(DT):1, lapply(.SD, function(x) !duplicated(cummax(x)))][nrow(DT): 1, ])
+# sum looks, sum visible trees
+sum((top + bottom + left + right) > 0)
+
+# part 2
+
+calculated <- function(x) {
+  sapply(seq.int(length(x)), function(i) {
+    val <- x[i]
+    check.v <- x[(i+1):nrow(DT)]
+    ans <- suppressWarnings(min(which(check.v >= val, arr.ind = TRUE)))
+    if(is.infinite(ans)) length(check.v) else ans
+  })
+}
+
+# looking down
+down <- DT[, lapply(.SD, calculated)]
+down[nrow(down), ] <- 0
+# looking up
+up <- DT[nrow(DT):1, lapply(.SD, calculated)]
+up[nrow(up), ] <- 0
+up <- up[nrow(DT):1, ]
+# looking left >> right
+left <- transpose(DT)[, lapply(.SD, calculated)]
+left[nrow(left), ] <- 0
+left <- transpose(left)
+# looking right >> left
+right <- transpose(DT)[nrow(DT):1, lapply(.SD, calculated)]
+right[nrow(right), ] <- 0
+right <- transpose(right[nrow(DT):1,])
+max(down * up * left * right)
